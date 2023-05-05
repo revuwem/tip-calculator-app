e9714f1b56ac8d1fb1a059c422a0ddf94931cc7f
Karina Grishaeva <revuwem@gmail.com>
2023-05-06 00:52:44 +0400

    fix(components/ui): Legend margin

--- src/components/atoms/Legend/Legend.tsx
+++ src/components/atoms/Legend/Legend.tsx
@@ -1,7 +1,7 @@
 type LegendProps = React.PropsWithChildren & {};
 
 const Legend: React.FC<LegendProps> = ({ children }) => {
-  return <legend className="text-sm text-cyan-grayish">{children}</legend>;
+  return <legend className="text-sm text-cyan-grayish mb-2">{children}</legend>;
 };
 
 export default Legend;
