5db08e9dc64643323c8d0aeab45c115ce3f97694
Karina Grishaeva <revuwem@gmail.com>
2023-05-05 20:42:26 +0400

    feat(components/atoms): create Input

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  className?: string;
+  icon?: React.ReactNode;
+};
+
+const Input: React.FC<InputProps> = ({ className = "", icon, ...props }) => {
+  return (
+    <div className="relative">
+      <input
+        className={clsx(
+          "py-1 px-3 pl-8 text-2xl text-right text-cyan-dark bg-cyan-very-light-grayish rounded transition",
+          "hover:outline hover:outline-2 hover:outline-cyan-strong hover:cursor-pointer",
+          "focus:outline focus:outline-2 focus:outline-cyan-strong",
+          className
+        )}
+        {...props}
+      />
+      {icon && (
+        <div className="absolute inset-0 w-3 aspect-square p-3">{icon}</div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
