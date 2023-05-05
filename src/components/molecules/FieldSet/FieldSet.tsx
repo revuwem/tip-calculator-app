a386581386a90c3a3484cf062faf917a2a8a8958
Karina Grishaeva <revuwem@gmail.com>
2023-05-06 00:29:13 +0400

    feat(components/molecules): add FieldSet mobile grid cols

--- src/components/molecules/FieldSet/FieldSet.tsx
+++ src/components/molecules/FieldSet/FieldSet.tsx
@@ -4,7 +4,11 @@ type FieldSetProps = React.PropsWithChildren & {};
 type FieldSetComponent = React.FC<FieldSetProps> & { Legend: FieldSetLegend };
 
 const FieldSet: FieldSetComponent = ({ children }) => {
-  return <fieldset className="grid grid-cols-3 gap-3">{children}</fieldset>;
+  return (
+    <fieldset className="grid grid-cols-2 xl:grid-cols-3 gap-3">
+      {children}
+    </fieldset>
+  );
 };
 
 type FieldSetLegend = typeof Legend;
