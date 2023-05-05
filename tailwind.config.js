c00c8dbcb5b82bb928691d4ddb53bfbe58277123
Karina Grishaeva <revuwem@gmail.com>
2023-05-05 19:25:42 +0400

    feat(tailwindcss): extend theme - add cyan colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          dark: "hsl(183, 100%, 15%)",
+          strong: "hsl(172, 67%, 45%)",
+          light: "hsl(173, 61%, 77%)",
+          "dark-grayish": "hsl(186, 14%, 43%)",
+          grayish: "hsl(184, 14%, 56%)",
+          "light-grayish": "hsl(185, 41%, 84%)",
+          "very-light-grayish": "hsl(189, 41%, 97%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
