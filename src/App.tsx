e22515b7ef4a32f77ccdb8e64151cf6bba0702d4
Karina Grishaeva <revuwem@gmail.com>
2023-05-06 01:16:39 +0400

    feat(App): create App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import TipsForm from "./components/organisms/TipsForm";
+import Logo from "./assets/logo.svg";
+import clsx from "clsx";
+
 function App() {
-  return <main>App</main>;
+  return (
+    <main
+      className={clsx(
+        "h-screen max-w-4xl mt-[15vh] mx-auto",
+        "grid grid-rows-[min-content_min-content] place-items-center gap-12 xl:gap-20"
+      )}
+    >
+      <Logo />
+      <TipsForm />
+    </main>
+  );
 }
 
 export default App;
