850bcfd0012c13c3e492ed905546be021ab64525
Karina Grishaeva <revuwem@gmail.com>
2023-05-08 00:20:05 +0400

    feat(components/molecules): add style for overflow total content

--- src/components/molecules/Total/Total.tsx
+++ src/components/molecules/Total/Total.tsx
@@ -5,11 +5,11 @@ type TotalProps = {
 
 const Total: React.FC<TotalProps> = ({ name, value }) => {
   return (
-    <section className="flex justify-between mb-8">
-      <h4 className="text-sm text-white">
+    <section className="flex justify-between mb-8 gap-4">
+      <h4 className="text-sm text-white shrink-0">
         {name} <span className="block text-xs text-cyan-grayish">/ person</span>
       </h4>
-      <p className="text-4xl text-cyan-strong">${value}</p>
+      <p className="text-4xl text-cyan-strong overflow-x-auto">${value}</p>
     </section>
   );
 };
